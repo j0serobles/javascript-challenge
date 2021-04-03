0c976bd273b8ed159e0003fca39af02bffe61492
j0serobles <engjoserobles@gmail.com>
2021-04-03 17:40:52 -0400

    Completed Both Levels

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -50,7 +50,7 @@ function fillLOV(elementID) {
   //Sort list 
   distinctValues.sort();
   //Append default value
-  distinctValues.unshift("<Select Value>");
+  distinctValues.unshift(" ");
 
   //Clear all options
   selectElement.selectAll("option").remove();
@@ -69,40 +69,65 @@ function runEnter() {
   
   // Select the start date's input element and get the raw HTML node
   var inputElement = d3.select("#sighting-date-start");
-
   // Get the value property of the search start date element
   var searchStartStr = inputElement.property("value");
-
   console.log(`Search Start Date = ${searchStartStr}`);
 
   // Select the end date's input element and get the raw HTML node
   inputElement = d3.select("#sighting-date-end");
-
   // Get the value property of the search start date element
   var  searchEndStr = inputElement.property("value");
+  console.log(`Search End Date   = ${searchEndStr}`);
 
-  console.log(`Search End Date   = ${searchEndStr}`  );
-  
-  console.log(sightings);
+  // Get a handle to the City list of Values element
+  inputElement = d3.select("#sighting-city");
+  // Get the value of the list of values
+  var searchCity =  inputElement.property("value");
+
+  // Get a handle to the State list of Values element
+  inputElement = d3.select("#sighting-state");
+  // Get the value of the list of values
+  var searchState =  inputElement.property("value");
+
+  // Get a handle to the Country list of Values element
+  inputElement = d3.select("#sighting-country");
+  // Get the value of the list of values
+  var searchCountry =  inputElement.property("value");
+
+  // Get a handle to the shape list of Values element
+  inputElement = d3.select("#sighting-shape");
+  // Get the value of the list of values
+  var searchShape =  inputElement.property("value");
+
+  console.log(`${searchStartStr}, ${searchEndStr}, ${searchCity}, ${searchState}, ${searchCountry}, ${searchShape}`);
 
   var filteredData = sightings.filter( sighting => {
     
     //Convert date strings to date type objects so comparison can be done.
     //Start by converting the sighting datetime to a Date object.
 
-    var sightingDate      = toDate(sighting.datetime)
-    var searchStartDate   = toDate(searchStartStr);
-    var searchEndDate     = toDate(searchEndStr);
+    var sightingDate      = toDate(sighting.datetime);
+    var searchStartDate   = searchStartStr ? toDate(searchStartStr) : toDate("01/01/0000");
+    var searchEndDate     = searchEndStr   ? toDate(searchEndStr)   : toDate("12/31/9999");
     
-     console.log(sightingDate);
-     console.log(searchStartDate);
-     console.log(searchEndDate);
-    
-    return( (sightingDate >= searchStartDate) && (sightingDate <= searchEndDate) ); 
+    console.log( 
+          (sightingDate     >=  (searchStartDate ?  searchStartDate : sightingDate ))
+      &&  (sightingDate     <=  (searchEndDate   ? searchEndDate : sightingDate))
+      &&  (sighting.city    === (searchCity      ? searchCity    : sighting.city))
+      &&  (sighting.state   === (searchState     ? searchState   : sighting.state))
+      &&  (sighting.country === (searchCountry   ? searchCountry : sighting.country))
+      &&  (sighting.shape   === (searchShape     ? searchShape   : sighting.shape))
+    );
 
+     return( (sightingDate >=  (searchStartDate ?  searchStartDate : sightingDate ))
+     &&  (sightingDate     <=  (searchEndDate   ? searchEndDate : sightingDate))
+     &&  (sighting.city    === (searchCity      ? searchCity    : sighting.city))
+     &&  (sighting.state   === (searchState     ? searchState   : sighting.state))
+     &&  (sighting.country === (searchCountry   ? searchCountry : sighting.country))
+     &&  (sighting.shape   === (searchShape     ? searchShape   : sighting.shape))
+   );
+   
   });
-
-  console.log(filteredData);
 
   //Select the table body element
   var sightingsTable = d3.select(".table.table-striped>tbody");
@@ -121,7 +146,7 @@ function runEnter() {
     table_row.append("td").text(row.comments); 
   });
 
-};
+}
 
 // Utility Function
 function toDate( dateString ){
